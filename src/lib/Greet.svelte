14057684485fba9fcbbea60aea7f0ec65f481bef
Nicolas Neudeck <nicolas.neudeck@outlook.de>
2025-02-28 15:17:57 +0100

    initial commit of skeleton app

---
+++ src/lib/Greet.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { invoke } from '@tauri-apps/api/core'
+
+  let name = ''
+  let greetMsg = ''
+
+  async function greet() {
+    console.log('Greeting', name)
+    greetMsg = await invoke('greet', { name })
+  }
+</script>
+
+<div>
+  <input id="greet-input" placeholder="Enter a name..." bind:value="{name}" />
+  <button on:click="{greet}">Greet</button>
+  <p>{greetMsg}</p>
+</div>
